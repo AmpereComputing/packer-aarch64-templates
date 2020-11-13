61084dd628926cf185e6130721278781516a02e3
Peter Pouliot <peter@pouliot.net>
2020-11-13 17:29:34 -0500

    Update build.sh

--- oraclelinux/build.sh
+++ oraclelinux/build.sh
@@ -8,4 +8,5 @@ packer build -on-error=abort -color=false -force oraclelinux-82-aarch64.json
 
 echo "convert to qcow2"
 qemu-img convert -c -O qcow2 packer-oraclelinux-r8u2-aarch64/oraclelinux-r8u2-aarch64 oraclelinux-r8u2-aarch64.qcow2
+sha256sum oraclelinux-r8u2-aarch64.qcow2
 
