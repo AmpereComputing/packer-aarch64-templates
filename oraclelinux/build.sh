b69c5e086ab7a2ae482ed7950922db0722cecb5a
Peter Pouliot <peter@pouliot.net>
2020-11-13 13:33:30 -0800

    working packer template

---
+++ oraclelinux/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo "Validate oraclelinux-82-aarch64.json"
+packer validate oraclelinux-82-aarch64.json
+
+echo "Build oraclelinux-82-aarch64.json"
+packer build -on-error=abort -color=false -force oraclelinux-82-aarch64.json
+
+echo "convert to qcow2"
+qemu-img convert -c -O qcow2 ./oraclelinux-r8u2-aarch64 ./oraclelinux-r8u2-aarch64.qcow2
+
